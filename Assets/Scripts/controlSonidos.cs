c8c1925ecab737770f175f298a66cf1ef65ccad6
giramos <uo202549@uniovi.es>
2020-11-07 10:39:28 +0100

    First commit :smile:

---
+++ Assets/Scripts/controlSonidos.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlSonidos : MonoBehaviour
+{
+    private AudioSource sonido;
+    public AudioClip mario;
+    public AudioClip tiro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sonido = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) // comprueba la tecla W
+        {
+            sonido.clip = mario;
+            sonido.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q)) // comprueba la tecla Q
+        {
+            sonido.clip = tiro; 
+            sonido.Play(); // Reproduce el sonido
+        }
+    }
+}
