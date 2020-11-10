66b6868947c535b356f99aaad69e6acd0ce39136
giramos <uo202549@uniovi.es>
2020-11-10 02:25:24 +0100

    :frog:First Commit:frog:

---
+++ Lost/Assets/Script/LogicaPersonaje.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaPersonaje : MonoBehaviour
+{
+    public float velocidadMovimiento = 5.0f;
+    public float velocidadRotacion = 200.0f;
+
+    private Animator anim;
+    private float x,y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
+
+        anim.SetFloat("VelX", x);
+        anim.SetFloat("VelY", y);
+    }
+}
