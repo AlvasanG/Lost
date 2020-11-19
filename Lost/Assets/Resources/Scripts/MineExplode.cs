0b5452eee348ac4f7bf8a2ae48a661ce2ec20f2e
Alvaro Sanchez <varosanchezgar5@gmail.com>
2020-11-19 23:42:34 +0100

    Progreso en alterar el terreno mediante minas
    Se ha logrado que el terreno debajo de la mina cambie de tamaño, osea cree un crater. Ahora es necesario buscar una manera de que eso funcione cuando la pises.

--- Lost/Assets/Resources/Scripts/MineExplode.cs
+++ Lost/Assets/Resources/Scripts/MineExplode.cs
@@ -1,37 +1,48 @@
 ﻿using System;
 using UnityEngine;
 
-public class MineExplode : MonoBehaviour
+namespace ObjectCode
 {
-    public GameObject explosionEffect;
-
-    public float radius = 10f;
-    public float explosionForce = 10f;
-
-    private void OnTriggerEnter(Collider other) {
         
-        if(other.tag == "Player"){
-            explosionEffect.SetActive(true);
-            Explode();      
+    public class MineExplode : MonoBehaviour
+    {
+        public GameObject explosionEffect;
+        public GameObject mine;
+
+        public float radius = 10f;
+        public float explosionForce = 10f;
+
+        private ObjectCode.TerrainFormer tFormer;
+
+        private void Start() {
+            tFormer = mine.AddComponent<ObjectCode.TerrainFormer>();
         }
 
-    }
-
-    private void Explode(){
-        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
-
-        foreach(Collider col in colliders){
-            Rigidbody rig = col.GetComponent<Rigidbody>();
-            if(rig != null){
-                // Realizar cualquier accion contra el jugador (col)
-                rig.AddExplosionForce(explosionForce, transform.position, radius, 2f, ForceMode.Impulse); // La explosion te manda volando
+        private void OnTriggerEnter(Collider other) {
+            
+            if(other.tag == "Player"){
+                explosionEffect.SetActive(true);
+                Explode();      
             }
+
         }
 
-        GameObject clone = Instantiate(explosionEffect, transform.position, transform.rotation);
-        ParticleSystem.MainModule particle = clone.GetComponent<ParticleSystem>().main;
-        Destroy(clone, particle.duration);
-        Destroy(gameObject);
+        private void Explode(){
+            Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+            foreach(Collider col in colliders){
+                Rigidbody rig = col.GetComponent<Rigidbody>();
+                if(rig != null){
+                    // Realizar cualquier accion contra el jugador (col)
+                    tFormer.UpdateHeightTerrain();
+                }
+            }
+
+            GameObject clone = Instantiate(explosionEffect, transform.position, transform.rotation);
+            ParticleSystem.MainModule particle = clone.GetComponent<ParticleSystem>().main;
+            Destroy(clone, particle.duration);
+            Destroy(gameObject);
+        }
     }
 
 }
---
+++ Lost/Assets/Resources/Scripts/TerrainFormer.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ObjectCode
+{
+    
+    public class TerrainFormer : MonoBehaviour {
+
+        private Terrain terr; // terrain to modify
+        private int hmWidth; // heightmap width
+        private int hmHeight; // heightmap height
+        
+        private int posXInTerrain; // position of the game object in terrain width (x axis)
+        private int posYInTerrain; // position of the game object in terrain height (z axis)
+        
+        public int size = 5; // the diameter of terrain portion that will raise under the game object
+        public float desiredHeight = 2f; // the height we want that portion of terrain to be
+
+        private float[,] originalHeights;
+
+        private Vector3 coordsMine;
+        
+        private void Start () {
+
+            terr = Terrain.activeTerrain;
+            hmWidth = terr.terrainData.heightmapResolution;
+            hmHeight = terr.terrainData.heightmapResolution;
+
+            this.originalHeights = terr.terrainData.GetHeights(0, 0, hmWidth, hmHeight);
+
+            Vector3 tempCoord = (transform.position - terr.gameObject.transform.position);
+            coordsMine.x = tempCoord.x / terr.terrainData.size.x;
+            coordsMine.y = tempCoord.y / terr.terrainData.size.y;
+            coordsMine.z = tempCoord.z / terr.terrainData.size.z;
+
+        }
+
+        public void UpdateHeightTerrain() {
+
+            float dHeight = desiredHeight / coordsMine.y;
+
+            // get the position of the terrain heightmap where this game object is
+            posXInTerrain = (int) (coordsMine.x * hmWidth); 
+            posYInTerrain = (int) (coordsMine.z * hmHeight);
+
+            // we set an offset so that all the raising terrain is under this game object
+            int offset = size / 2;
+        
+            // get the heights of the terrain under this game object
+            float[,] heights = terr.terrainData.GetHeights(posXInTerrain-offset,posYInTerrain-offset,size,size);
+        
+            // we set each sample of the terrain in the size to the desired height
+            for (int i=0; i < size; i++)
+                for (int j=0; j < size; j++)
+                    heights[i,j] = dHeight;
+        
+            // set the new height
+            terr.terrainData.SetHeights(posXInTerrain-offset,posYInTerrain-offset,heights);
+
+        }
+
+        private void Update() {
+
+            //float dHeight = desiredHeight / terr.terrainData.size.y;
+        
+            // get the normalized position of this game object relative to the terrain
+            Vector3 tempCoord = (transform.position - terr.gameObject.transform.position);
+            Vector3 coord;
+            coord.x = tempCoord.x / terr.terrainData.size.x;
+            coord.y = tempCoord.y / terr.terrainData.size.y;
+            coord.z = tempCoord.z / terr.terrainData.size.z;
+        
+            // get the position of the terrain heightmap where this game object is
+            //posXInTerrain = (int) (coord.x * hmWidth); 
+            //posYInTerrain = (int) (coord.z * hmHeight);
+        
+            // we set an offset so that all the raising terrain is under this game object
+            //int offset = size / 2;
+        
+            // get the heights of the terrain under this game object
+            //float[,] heights = terr.terrainData.GetHeights(posXInTerrain-offset,posYInTerrain-offset,size,size);
+        
+            // we set each sample of the terrain in the size to the desired height
+            /*for (int i=0; i < size; i++)
+                for (int j=0; j < size; j++)
+                    heights[i,j] = dHeight;
+        
+            // set the new height
+            terr.terrainData.SetHeights(posXInTerrain-offset,posYInTerrain-offset,heights);
+            */
+        }
+
+        private void OnDisable(){
+            if(terr != null)
+                terr.terrainData.SetHeights(0, 0, this.originalHeights);
+        }
+    }
+
+}
