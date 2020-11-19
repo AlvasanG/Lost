fd611ad940d2892fd463889de116329d9587b973
Alvaro Sanchez <varosanchezgar5@gmail.com>
2020-11-19 22:04:17 +0100

    Añadida Mina Terrestre
    Añadida una mina terrestre que explota cuando el jugador pasa encima de ella. Actualmente explota y envia al jugador volando, pero puede ser implementado otro tipo de acciones.

---
+++ Lost/Assets/Resources/Scripts/MineExplode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class MineExplode : MonoBehaviour
+{
+    public GameObject explosionEffect;
+
+    public float radius = 10f;
+    public float explosionForce = 10f;
+
+    private void OnTriggerEnter(Collider other) {
+        
+        if(other.tag == "Player"){
+            explosionEffect.SetActive(true);
+            Explode();      
+        }
+
+    }
+
+    private void Explode(){
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach(Collider col in colliders){
+            Rigidbody rig = col.GetComponent<Rigidbody>();
+            if(rig != null){
+                // Realizar cualquier accion contra el jugador (col)
+                rig.AddExplosionForce(explosionForce, transform.position, radius, 2f, ForceMode.Impulse); // La explosion te manda volando
+            }
+        }
+
+        GameObject clone = Instantiate(explosionEffect, transform.position, transform.rotation);
+        ParticleSystem.MainModule particle = clone.GetComponent<ParticleSystem>().main;
+        Destroy(clone, particle.duration);
+        Destroy(gameObject);
+    }
+
+}
