369f2fdbbc336ab46da1a0711ca8952c16b48e8d
Alvaro Sanchez <varosanchezgar5@gmail.com>
2020-11-26 14:30:13 +0100

    Bunqueres tienen HP
    Los bunqueres tiene vida y desaparecen cuando la vida se agota. Además la barra de vida sigue a la camara del jugador.

---
+++ Lost/Assets/Resources/Scripts/BunkerHP.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BunkerHP : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+
+
+    public GameObject healthBarUI;
+    public Slider hpBar;
+
+
+    void Start(){
+        health = maxHealth;
+        hpBar.value = CalculateHealth();
+    }
+
+    void Update(){
+        hpBar.value = CalculateHealth();
+
+        if (health < maxHealth){
+            healthBarUI.SetActive(true);
+        }
+        if(health <= 0){
+            Destroy(gameObject);
+        }
+
+        if(health > maxHealth){
+            health = maxHealth;
+        }
+    }
+
+    float CalculateHealth(){
+        return health / maxHealth;
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.tag == "grenade"){
+            health -= (health/3);
+        }
+    }
+}
