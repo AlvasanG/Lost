f30e5f89e3f107877ea32ac900cb29e6ff06d93e
Alvaro Sanchez <varosanchezgar5@gmail.com>
2020-11-26 20:31:04 +0100

    Poblado nivel de trincheras
    Añadidos bunkeres y granadas al nivel. Añadido muro invisible al final del nivel.

---
+++ Lost/Assets/Resources/Scripts/FinishLevelTrench.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FinishLevelTrench : MonoBehaviour
+{
+
+    public GameObject[] bunkersStanding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bunkersStanding = GameObject.FindGameObjectsWithTag("Bunker");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bunkersStanding = GameObject.FindGameObjectsWithTag("Bunker");
+        if(bunkersStanding.Length == 0){
+            Destroy(gameObject);
+        }
+    }
+}
