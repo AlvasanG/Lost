9969b9ffbbd12252c81e0fa2bdfe0ca88374f07b
Fernando Giganto Rodriguez <fernandogigantorodriguez@gmail.com>
2020-12-08 10:48:50 +0100

    scripts enemigos
    nuevos scripts enemigos

---
+++ Lost/Assets/Resources/Scripts/Disparo.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour
+{
+
+
+    public Transform Player;
+    public int damp = 2;
+    public Rigidbody projectile;
+    public float speed = 20f;
+    public float temp = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Player.position - transform.position), 
+                                              damp * Time.deltaTime);
+
+        if (temp > 0)
+        {
+            temp -= Time.deltaTime;
+        }
+        else
+        {
+            if (Vector3.Distance(Player.position, transform.position) < 30)
+            {
+                Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation);
+
+                instantiatedProjectile.velocity = transform.TransformDirection(0, 0, speed);
+
+                Physics.IgnoreCollision(instantiatedProjectile.GetComponent<Collider>(), transform.root.GetComponent<Collider>());
+                temp = 3.0f;
+            }   
+        }
+    }
+}
+
+
+
+
+
+
+ 
---
+++ Lost/Assets/Resources/Scripts/Perseguidor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perseguidor : MonoBehaviour
+{
+
+    public float velocidadePerseguidor = 5f;
+    public Transform Player;
+    public int damp = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        if (Player != null)
+        {
+
+            if (Vector3.Distance(Player.position, transform.position) < 50)
+            {
+                Quaternion rotation = Quaternion.LookRotation(Player.position - transform.position);
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, damp * Time.deltaTime);
+                transform.Translate(0, 0, velocidadePerseguidor * Time.deltaTime);
+            }
+        }
+    }
+}
