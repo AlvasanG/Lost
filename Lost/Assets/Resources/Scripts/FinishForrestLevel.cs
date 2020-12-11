fe77a6f8bae1623b717c7745fe591ceedb2437ba
Fernando Giganto Rodriguez <fernandogigantorodriguez@gmail.com>
2020-12-11 10:34:43 +0100

    script
    script

--- Lost/Assets/Resources/Scripts/FinishForrestLevel.cs
+++ Lost/Assets/Resources/Scripts/FinishForrestLevel.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FinishForrestLevel : MonoBehaviour
+
+    private BoxCollider col;
+    private MainMenu menuBehaviour;
+
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        menuBehaviour = GameObject.FindWithTag("Menu").GetComponent<MainMenu>();
+        col = GetComponent<BoxCollider>();
+        col.isTrigger = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            menuBehaviour.EscenaJuego("Iglesia");
+        }
     }
 }
