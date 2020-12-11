ec6d07636eabe2b0b9a5b9a071f7be43d554c544
Alvaro Sanchez <varosanchezgar5@gmail.com>
2020-12-11 11:35:01 +0100

    Añadido cambio de nivel bosque
    Añadido el cambio del nivel bosque a el nivel Iglesia. Añadido componente VR al bosque e iglesia

--- Lost/Assets/Resources/Scripts/FinishForrestLevel.cs
+++ Lost/Assets/Resources/Scripts/FinishForrestLevel.cs
@@ -3,23 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FinishForrestLevel : MonoBehaviour
+{    private MainMenu menuBehaviour;
 
-    private BoxCollider col;
-    private MainMenu menuBehaviour;
-
-{
     // Start is called before the first frame update
     void Start()
     {
         menuBehaviour = GameObject.FindWithTag("Menu").GetComponent<MainMenu>();
-        col = GetComponent<BoxCollider>();
-        col.isTrigger = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     void OnTriggerEnter(Collider other)
